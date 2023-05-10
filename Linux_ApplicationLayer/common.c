3745d1cf73778d06a2f159b5c97d47661d6780db
lilone2569 <110813583+lilone2569@users.noreply.github.com>
2023-05-10 18:45:10 +0800

    Update common.c

--- Linux_ApplicationLayer/common.c
+++ Linux_ApplicationLayer/common.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//编写带参数的main，实现简易的计算器的功能，能够实现加减乘除。
+int main(int argc,char *argv[])
+{
+	int a=atoi(argv[1]);//把字符串转换成数字
+	int b=atoi(argv[3]);
+	
+	switch(*argv[2])
+	{
+		case '+':printf("%d + %d = %d\r\n",a,b,a+b);break;
+		case '-':printf("%d - %d = %d\r\n",a,b,a-b);break;
+		case '*':printf("%d * %d = %d\r\n",a,b,a*b);break;
+		case '/':printf("%d / %d = %d\r\n",a,b,a/b);break;
+	}
+	
+	return 0;
+}
