241731c9ef70d35303b7ab9fe38fc3e41866a123
lilone2569 <110813583+lilone2569@users.noreply.github.com>
2023-05-10 19:22:09 +0800

    Update FileIO.c

--- Linux_ApplicationLayer/FileIO.c
+++ Linux_ApplicationLayer/FileIO.c
@@ -4,14 +4,19 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+#include <unistd.h>
 
 //1.int open(const char *pathname, int flags);
 //flags:O_RDONLY, O_WRONLY, or O_RDWR
 //O_CREAT,  O_DIRECTORY,  O_EXCL,  O_NOCTTY,  O_NOFOLLOW,  O_TMPFILE, andO_TRUNC.
+//return:返回一个文件描述符fd
+
+//2.ssize_t write(int fd, const void *buf, size_t count);
 
 
+//3.ssize_t read(int fd, void *buf, size_t count);
 
+//4.close(int fd);
 
 int main(int argc,char *argv[])
 {
