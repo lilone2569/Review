77cb96fc42308d24157bca15f986b80afb061873
lilone2569 <110813583+lilone2569@users.noreply.github.com>
2023-05-11 09:44:52 +0800

    Update 仿写cp命令.c

--- Linux_ApplicationLayer/FileIO/仿写cp命令.c
+++ Linux_ApplicationLayer/FileIO/仿写cp命令.c
@@ -13,13 +13,13 @@ int main(int argc,char *argv[])
 {
     int fd1,fd2;
     char buffer[200];
-    int len = 0;
+    int len = 0,ret;
     fd1 = open(argv[1], O_RDONLY);
     if(fd1 < 0){
         perror("open first file failed!\n");
         return ERROR;
     }
-    fd2 = open(argv[2], O_RDWR | O_CREAT |0666);
+    fd2 = open(argv[2], O_RDWR | O_CREAT |O_TRUNC,0666); // 可读可写可创建—以覆盖的方式
     if(fd1 < 0){
         perror("open second file failed!\n");
         return ERROR;
@@ -27,19 +27,29 @@ int main(int argc,char *argv[])
     while(1){
         len = read(fd1,buffer,sizeof(buffer)-1);
         if(len < 0){
-            perror("read first file failed!\n");
+            perror("read to buffer  failed!\n");
             return ERROR;
         }
         else if(len == 0) break;
-        write(fd2,buffer,sizeof(buffer));
-        
-
-
+        ret = write(fd2,buffer,strlen(buffer));
+        if(ret == -1){
+            perror("write to first file failed!\n");
+            return ERROR;
+        }
     }
-
+    memset(buffer,0,sizeof(buffer));
+    close(fd1);
+    if(fd1 == -1){
+        perror("close first file failed!\n");
+        return ERROR;
+    }
+    close(fd2);
+    if(fd2 == -1){
+        perror("close second file failed!\n");
+        return ERROR;
+    }
     return 0;
 }
-
 
 
 
