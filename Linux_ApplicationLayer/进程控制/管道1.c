f446650337b36545f8372bfea1796a3182d7af61
lilone2569 <110813583+lilone2569@users.noreply.github.com>
2023-05-11 11:34:27 +0800

    Update 管道1.c
    添加注释

--- Linux_ApplicationLayer/进程控制/管道1.c
+++ Linux_ApplicationLayer/进程控制/管道1.c
@@ -15,12 +15,12 @@
 int main(int argc,char *argv[])
 {
     int pipefd[2] = {0};
-    int ret = pipe(pipefd);
+    int ret = pipe(pipefd);  //创建管道
     if(ret == -1){
         perror("create No name pipe failed\n");
         return ERROR;
     }
-    int pid = fork();
+    int pid = fork();   //创建子进程
     if(pid == -1){
         perror("create process failed\n");
         return ERROR;
@@ -31,12 +31,12 @@ int main(int argc,char *argv[])
         while(1){
             printf("请输入要写入管道的数据：");
             fgets(buf,sizeof(buf),stdin);
-            ret = write(pipefd[1],buf,strlen(buf));
+            ret = write(pipefd[1],buf,strlen(buf));    //将输入的数据写入管道
             if(ret == -1){
                 perror("write pipe failed\n");
                 return ERROR;
             }
-            usleep(10);
+            usleep(10);  //经过测试发现父进程跑完还是被父进程抢占所以短暂挂起一下
         }
         wait(NULL);
     }
@@ -44,7 +44,7 @@ int main(int argc,char *argv[])
         char buf[50];
         int ret;
         while(1){
-            ret = read(pipefd[0],buf,sizeof(buf)-1);
+            ret = read(pipefd[0],buf,sizeof(buf)-1);  //将管道的数据读到缓存区
             if(ret < 0){
                 perror("read from pipe failed\n");
                 return 0;
