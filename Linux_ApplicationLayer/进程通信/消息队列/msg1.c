a87434b4bf1f17fd2e2ac1879f78b4bcca6c8533
lilone2569 <110813583+lilone2569@users.noreply.github.com>
2023-05-18 18:04:50 +0800

    Update msg1.c

--- Linux_ApplicationLayer/进程通信/消息队列/msg1.c
+++ Linux_ApplicationLayer/进程通信/消息队列/msg1.c
@@ -30,7 +30,7 @@ int main(int argc,char *argv[])
         perror("get key failed!");
         return ERROR;
     }
-    int msgid = msgget(key,IPC_CREAT|0666);
+    int msgid = msgget(key,IPC_CREAT|0666);   //获取消息队列id号，需要添加可读写权限否则无法读写
     if(msgid == -1){
         perror("get msgid failed!");
         return ERROR;
@@ -49,12 +49,12 @@ int main(int argc,char *argv[])
             printf("Please input message:");
             fgets(buf1.mtext,sizeof(buf1.mtext),stdin);
             printf("buf1.mtext=%s\n",buf1.mtext);
-            ret = msgsnd(msgid,&buf1,strlen(buf1.mtext),0);
+            ret = msgsnd(msgid,&buf1,strlen(buf1.mtext),0);   //发送消息到队列
             if(ret == -1){
                 perror("message send failed!\n");
                 return ERROR;
             }
-            if(strcmp(buf1.mtext,"quit\n") == 0) break;
+            if(strcmp(buf1.mtext,"quit\n") == 0) break;   //输入quit则退出循环
 
             usleep(10);
             memset(buf1.mtext,0,sizeof(buf1.mtext));
@@ -65,7 +65,7 @@ int main(int argc,char *argv[])
         struct msgbuf buf2;
         buf2.mtype = 1;
         while(1){
-            if(msgrcv(msgid,&buf2,sizeof(buf2.mtext),1,0) == -1){
+            if(msgrcv(msgid,&buf2,sizeof(buf2.mtext),1,0) == -1){   //接受消息队列的消息
                 perror("message receive error!\n");
                 return ERROR;
             }
